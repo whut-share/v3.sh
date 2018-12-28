e31ccc0523407c445cfe11eb4969a71a93b05f80
oldboys <oldboys@cn.com>
2018-12-28 17:16:45 +0800

    update readme and refine script.
    Signed-off-by: oldboys <oldboys@cn.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# v3.sh
+# v6.sh
+wget https://raw.githubusercontent.com/whut-share/v6/master/v6.sh
+
+### bbr_plus
+
+wget "https://github.com/cx9208/bbrplus/raw/master/ok_bbrplus_centos.sh" && chmod +x ok_bbrplus_centos.sh && ./ok_bbrplus_centos.sh
+
+安装后，执行uname -r，显示4.14.89则切换内核成功
+执行lsmod | grep bbr，显示有bbrplus则开启成功
--- v6.sh
+++ v6.sh
@@ -157,14 +157,15 @@ pm2_list(){
 }
 
 install_pm2(){
-        #检查系统版本
-        check_sys
+    #检查系统版本
+    check_sys
 
 	#判断/usr/bin/pm2文件是否存在
 	if [ ! -f /usr/bin/pm2 ]; then
         echo "检查到您未安装pm2,脚本将先进行安装"
         #安装Node.js
         if [[ ${release} = "centos" ]]; then
+	        yum -y update
             yum -y install xz
             yum -y install wget
             wget -N https://github.com/Super-box/v3/raw/master/resolv.conf -P /etc && /usr/bin/chattr +i /etc/resolv.conf
@@ -204,7 +205,6 @@ install_pm2(){
 use_centos_pm2(){
     if [ ! -f /usr/bin/killall ]; then
 	    echo "检查到您未安装psmisc,脚本将先进行安装"
-	    yum -y update
 	    yum -y install psmisc
     fi
 
@@ -1136,158 +1136,157 @@ uninstall_ali_cloud_shield(){
 	echo "请选择：[1]卸载阿里云盾 [2]卸载腾讯云盾";read uninstall_ali_cloud_shield
 
 	if [ ${uninstall_ali_cloud_shield} = '1' ]; then
-    yum -y install redhat-lsb
-       var=`lsb_release -a | grep Gentoo`
-    if [ -z "${var}" ]; then
-	   var=`cat /etc/issue | grep Gentoo`
-    fi
-
-    if [ -d "/etc/runlevels/default" -a -n "${var}" ]; then
-	   LINUX_RELEASE="GENTOO"
-    else
-	   LINUX_RELEASE="OTHER"
-    fi
-
-    stop_aegis(){
-	killall -9 aegis_cli >/dev/null 2>&1
-	killall -9 aegis_update >/dev/null 2>&1
-	killall -9 aegis_cli >/dev/null 2>&1
-	killall -9 AliYunDun >/dev/null 2>&1
-	killall -9 AliHids >/dev/null 2>&1
-	killall -9 AliYunDunUpdate >/dev/null 2>&1
-    printf "%-40s %40s\n" "Stopping aegis" "[  OK  ]"
-    }
-
-    remove_aegis(){
-    if [ -d /usr/local/aegis ]; then
-       rm -rf /usr/local/aegis/aegis_client
-       rm -rf /usr/local/aegis/aegis_update
-	   rm -rf /usr/local/aegis/alihids
-    fi
-    }
-
-   uninstall_service() {
-
-   if [ -f "/etc/init.d/aegis" ]; then
-		/etc/init.d/aegis stop  >/dev/null 2>&1
-		rm -f /etc/init.d/aegis
-   fi
-
-	if [ $LINUX_RELEASE = "GENTOO" ]; then
-		rc-update del aegis default 2>/dev/null
-		if [ -f "/etc/runlevels/default/aegis" ]; then
-			rm -f "/etc/runlevels/default/aegis" >/dev/null 2>&1;
-		fi
-    elif [ -f /etc/init.d/aegis ]; then
-         /etc/init.d/aegis  uninstall
-	    for ((var=2; var<=5; var++)) do
-			if [ -d "/etc/rc${var}.d/" ]; then
-				 rm -f "/etc/rc${var}.d/S80aegis"
-		    elif [ -d "/etc/rc.d/rc${var}.d" ]; then
-				rm -f "/etc/rc.d/rc${var}.d/S80aegis"
-			fi
-		done
-    fi
-
-    }
-
-    stop_aegis
-    uninstall_service
-    remove_aegis
-
-    printf "%-40s %40s\n" "Uninstalling aegis"  "[  OK  ]"
-
-    var=`lsb_release -a | grep Gentoo`
-    if [ -z "${var}" ]; then
-    	var=`cat /etc/issue | grep Gentoo`
-    fi
-
-    if [ -d "/etc/runlevels/default" -a -n "${var}" ]; then
-    	LINUX_RELEASE="GENTOO"
-    else
-    	LINUX_RELEASE="OTHER"
-    fi
-
-    stop_aegis(){
-    	killall -9 aegis_cli >/dev/null 2>&1
-    	killall -9 aegis_update >/dev/null 2>&1
-    	killall -9 aegis_cli >/dev/null 2>&1
-        printf "%-40s %40s\n" "Stopping aegis" "[  OK  ]"
-    }
-
-    stop_quartz(){
-    	killall -9 aegis_quartz >/dev/null 2>&1
-            printf "%-40s %40s\n" "Stopping quartz" "[  OK  ]"
-    }
-
-    remove_aegis(){
-    if [ -d /usr/local/aegis ]; then
-        rm -rf /usr/local/aegis/aegis_client
-        rm -rf /usr/local/aegis/aegis_update
-    fi
-    }
-
-    remove_quartz(){
-    if [ -d /usr/local/aegis ]; then
-    	rm -rf /usr/local/aegis/aegis_quartz
-    fi
-    }
-
-
-    uninstall_service() {
-
-       if [ -f "/etc/init.d/aegis" ]; then
-    		/etc/init.d/aegis stop  >/dev/null 2>&1
-    		rm -f /etc/init.d/aegis
-       fi
-
-    	if [ $LINUX_RELEASE = "GENTOO" ]; then
-    		rc-update del aegis default 2>/dev/null
-    		if [ -f "/etc/runlevels/default/aegis" ]; then
-    			rm -f "/etc/runlevels/default/aegis" >/dev/null 2>&1;
-    		fi
-        elif [ -f /etc/init.d/aegis ]; then
-             /etc/init.d/aegis  uninstall
-    	    for ((var=2; var<=5; var++)) do
-    			if [ -d "/etc/rc${var}.d/" ]; then
-    				 rm -f "/etc/rc${var}.d/S80aegis"
-    		    elif [ -d "/etc/rc.d/rc${var}.d" ]; then
-    				rm -f "/etc/rc.d/rc${var}.d/S80aegis"
-    			fi
-    		done
+        yum -y install redhat-lsb
+        var=`lsb_release -a | grep Gentoo`
+        if [ -z "${var}" ]; then
+	        var=`cat /etc/issue | grep Gentoo`
         fi
 
-    }
-            stop_aegis
-            stop_quartz
-            uninstall_service
-            remove_aegis
-            remove_quartz
-            printf "%-40s %40s\n" "Uninstalling aegis_quartz"  "[  OK  ]"
-            pkill aliyun-service
-            rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
-            rm -rf /usr/local/aegis*
-            iptables -I INPUT -s 140.205.201.0/28 -j DROP
-            iptables -I INPUT -s 140.205.201.16/29 -j DROP
-            iptables -I INPUT -s 140.205.201.32/28 -j DROP
-            iptables -I INPUT -s 140.205.225.192/29 -j DROP
-            iptables -I INPUT -s 140.205.225.200/30 -j DROP
-            iptables -I INPUT -s 140.205.225.184/29 -j DROP
-            iptables -I INPUT -s 140.205.225.183/32 -j DROP
-            iptables -I INPUT -s 140.205.225.206/32 -j DROP
-            iptables -I INPUT -s 140.205.225.205/32 -j DROP
-            iptables -I INPUT -s 140.205.225.195/32 -j DROP
-            iptables -I INPUT -s 140.205.225.204/32 -j DROP
-        elif [ ${uninstall_ali_cloud_shield} = '2' ]; then
-        	#检查文件uninstal_qcloud.sh是否存在,若不存在,则下载该文件
+        if [ -d "/etc/runlevels/default" -a -n "${var}" ]; then
+	        LINUX_RELEASE="GENTOO"
+        else
+	        LINUX_RELEASE="OTHER"
+        fi
+
+        stop_aegis(){
+	        killall -9 aegis_cli >/dev/null 2>&1
+	        killall -9 aegis_update >/dev/null 2>&1
+	        killall -9 aegis_cli >/dev/null 2>&1
+	        killall -9 AliYunDun >/dev/null 2>&1
+	        killall -9 AliHids >/dev/null 2>&1
+	        killall -9 AliYunDunUpdate >/dev/null 2>&1
+            printf "%-40s %40s\n" "Stopping aegis" "[  OK  ]"
+        }
+
+        remove_aegis(){
+            if [ -d /usr/local/aegis ]; then
+                rm -rf /usr/local/aegis/aegis_client
+                rm -rf /usr/local/aegis/aegis_update
+	            rm -rf /usr/local/aegis/alihids
+            fi
+        }
+
+        uninstall_service() {
+
+            if [ -f "/etc/init.d/aegis" ]; then
+		        /etc/init.d/aegis stop  >/dev/null 2>&1
+		        rm -f /etc/init.d/aegis
+            fi
+
+	        if [ $LINUX_RELEASE = "GENTOO" ]; then
+		        rc-update del aegis default 2>/dev/null
+		        if [ -f "/etc/runlevels/default/aegis" ]; then
+			        rm -f "/etc/runlevels/default/aegis" >/dev/null 2>&1;
+		        fi
+            elif [ -f /etc/init.d/aegis ]; then
+                /etc/init.d/aegis  uninstall
+	            for ((var=2; var<=5; var++)) do
+			        if [ -d "/etc/rc${var}.d/" ]; then
+				        rm -f "/etc/rc${var}.d/S80aegis"
+		            elif [ -d "/etc/rc.d/rc${var}.d" ]; then
+				        rm -f "/etc/rc.d/rc${var}.d/S80aegis"
+			        fi
+		        done
+                fi
+
+        }
+
+        stop_aegis
+        uninstall_service
+        remove_aegis
+
+        printf "%-40s %40s\n" "Uninstalling aegis"  "[  OK  ]"
+
+        var=`lsb_release -a | grep Gentoo`
+        if [ -z "${var}" ]; then
+    	    var=`cat /etc/issue | grep Gentoo`
+        fi
+
+        if [ -d "/etc/runlevels/default" -a -n "${var}" ]; then
+    	    LINUX_RELEASE="GENTOO"
+        else
+    	    LINUX_RELEASE="OTHER"
+        fi
+
+        stop_aegis(){
+    	    killall -9 aegis_cli >/dev/null 2>&1
+    	    killall -9 aegis_update >/dev/null 2>&1
+    	    killall -9 aegis_cli >/dev/null 2>&1
+            printf "%-40s %40s\n" "Stopping aegis" "[  OK  ]"
+        }
+
+        stop_quartz(){
+    	    killall -9 aegis_quartz >/dev/null 2>&1
+            printf "%-40s %40s\n" "Stopping quartz" "[  OK  ]"
+        }
+
+        remove_aegis(){
+            if [ -d /usr/local/aegis ]; then
+                rm -rf /usr/local/aegis/aegis_client
+                rm -rf /usr/local/aegis/aegis_update
+            fi
+        }
+
+        remove_quartz(){
+            if [ -d /usr/local/aegis ]; then
+    	        rm -rf /usr/local/aegis/aegis_quartz
+            fi
+        }
+
+        uninstall_service() {
+
+            if [ -f "/etc/init.d/aegis" ]; then
+    		    /etc/init.d/aegis stop  >/dev/null 2>&1
+    		    rm -f /etc/init.d/aegis
+            fi
+
+    	    if [ $LINUX_RELEASE = "GENTOO" ]; then
+    		    rc-update del aegis default 2>/dev/null
+    		    if [ -f "/etc/runlevels/default/aegis" ]; then
+    			    rm -f "/etc/runlevels/default/aegis" >/dev/null 2>&1;
+    		    fi
+            elif [ -f /etc/init.d/aegis ]; then
+                /etc/init.d/aegis  uninstall
+    	        for ((var=2; var<=5; var++)) do
+    			    if [ -d "/etc/rc${var}.d/" ]; then
+    				    rm -f "/etc/rc${var}.d/S80aegis"
+    		        elif [ -d "/etc/rc.d/rc${var}.d" ]; then
+    				    rm -f "/etc/rc.d/rc${var}.d/S80aegis"
+    			    fi
+    		    done
+                fi
+
+        }
+        stop_aegis
+        stop_quartz
+        uninstall_service
+        remove_aegis
+        remove_quartz
+        printf "%-40s %40s\n" "Uninstalling aegis_quartz"  "[  OK  ]"
+        pkill aliyun-service
+        rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+        rm -rf /usr/local/aegis*
+        iptables -I INPUT -s 140.205.201.0/28 -j DROP
+        iptables -I INPUT -s 140.205.201.16/29 -j DROP
+        iptables -I INPUT -s 140.205.201.32/28 -j DROP
+        iptables -I INPUT -s 140.205.225.192/29 -j DROP
+        iptables -I INPUT -s 140.205.225.200/30 -j DROP
+        iptables -I INPUT -s 140.205.225.184/29 -j DROP
+        iptables -I INPUT -s 140.205.225.183/32 -j DROP
+        iptables -I INPUT -s 140.205.225.206/32 -j DROP
+        iptables -I INPUT -s 140.205.225.205/32 -j DROP
+        iptables -I INPUT -s 140.205.225.195/32 -j DROP
+        iptables -I INPUT -s 140.205.225.204/32 -j DROP
+    elif [ ${uninstall_ali_cloud_shield} = '2' ]; then
+        #检查文件uninstal_qcloud.sh是否存在,若不存在,则下载该文件
 	    if [ ! -f /root/uninstal_qcloud.sh ]; then
 	    	curl -sSL https://down.oldking.net/Script/uninstal_qcloud.sh
 	    	chmod +x uninstal_qcloud.sh
 	    fi
-            sudo bash uninstal_qcloud.sh
-    	else
-    		echo "选项不在范围内,更新中止.";exit 0
-    	fi
+        sudo bash uninstal_qcloud.sh
+    else
+    	echo "选项不在范围内,更新中止.";exit 0
+    fi
 }
 
 #回程路由测试-[b]
